b9788fc34e33125d0fa3c6f90949ccdd78a61619
ichirosato <ivometal_1992@abv.bg>
2023-06-21 16:38:04 +0300

    Add method PerimeterOfRectangle

--- DemoVS/DemoVS/Program.cs
+++ DemoVS/DemoVS/Program.cs
@@ -6,12 +6,18 @@ namespace DemoVS
     {
         static void Main(string[] args)
         {
-            double Calc = AreaOfRectangle(6,5);
-            Console.WriteLine(Calc);
+            double CalcTheFirst = AreaOfRectangle(6,5);
+            double CalcTheSecond = AreaOfRectangle(6,5);
+            Console.WriteLine(CalcTheFirst);
+            Console.WriteLine(CalcTheSecond);
         }
         public static double AreaOfRectangle(double a, double b)
         {
             return a * b;
+        }
+        public static double PerimeterOfRectangle(double a, double b)
+        {
+            return 2 * a + 2 * b;
         }
     }
 }
